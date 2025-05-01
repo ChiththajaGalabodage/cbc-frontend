7253b74b5ea199a48abb4cd8bfb19372b08e6c4a
Chiththaja <162040367+ChiththajaGalabodage@users.noreply.github.com>
2025-05-01 12:03:07 +0530

    learn position

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,17 @@ import ProductCard from "./components/productCard";
 
 function App() {
   return (
-   
+    <div className="w-full h-screen bg-red-100 flex flex-col justify-center items-center">
+      <div className="w-[650px] h-[650px] bg-red-900 flex flex-col justify-center items-center">
+        <div className="w-[600px] h-[600px] bg-green-600 flex flex-col justify-center items-center">
+          <div className="w-[100px] h-[100px] bg-red-600"></div>
+          <div className="w-[100px] h-[100px] bg-blue-600 fixed"></div>
+          <div className="w-[100px] h-[100px] bg-pink-600"></div>
+          <div className="w-[100px] h-[100px] bg-yellow-400"></div>
+          <div className="w-[100px] h-[100px] bg-orange-500"></div>
+        </div>
+      </div>
+    </div>
   );
 }
 
