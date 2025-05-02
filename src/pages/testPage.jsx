e782ce23eda59db242d7665a56bd360b8f9647d4
Chiththaja <162040367+ChiththajaGalabodage@users.noreply.github.com>
2025-05-02 12:16:21 +0530

    useState hock 📍

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
+
 export default function TestPage() {
-  let count = 0;
+  const [count, setCount] = useState(0);
+
   return (
     <div className="w-full h-screen  flex justify-center items-center">
       <div className="w-[450px] h-[250px] shadow flex justify-center items-center">
         <button
           onClick={() => {
-            console.log("- clicked");
+            setCount(count - 1);
           }}
           className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] mx-[20px] cusor-pointer"
         >
@@ -16,7 +19,7 @@ export default function TestPage() {
         </span>
         <button
           onClick={() => {
-            console.log("+ clicked");
+            setCount(count + 1);
           }}
           className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cusor-pointer"
         >
