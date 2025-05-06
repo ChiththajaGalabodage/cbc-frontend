dda6b3c91c2cde5bead03c1a85ab35469770792e
Chiththaja <162040367+ChiththajaGalabodage@users.noreply.github.com>
2025-05-07 01:43:40 +0530

    implement products data table

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -18,33 +18,20 @@ export default function AdminProductsPage() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>COSM24003</td>
-            <td>Charcoal Face Mask</td>
-            <td>
-              <img
-                src="https://example.com/images/charcoal_mask.jpg"
-                className="w-[50px] h-[50px]"
-              />
-            </td>
-            <td>20</td>
-            <td>16.75</td>
-            <td>60</td>
-          </tr>
-
-          <tr>
-            <td>COSM24003</td>
-            <td>Charcoal Face Mask</td>
-            <td>
-              <img
-                src="https://example.com/images/charcoal_mask.jpg"
-                className="w-[50px] h-[50px]"
-              />
-            </td>
-            <td>20</td>
-            <td>16.75</td>
-            <td>60</td>
-          </tr>
+          {products.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item.productId}</td>
+                <td>{item.name}</td>
+                <td>
+                  <img src={item.images[0]} className="w-[50px] h-[50px]" />
+                </td>
+                <td>{item.labelledPrice}</td>
+                <td>{item.price}</td>
+                <td>{item.stock}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
