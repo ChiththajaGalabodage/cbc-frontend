1ee539c5352f91db8c088cba7e6a8004b74b85fc
Chiththaja <162040367+ChiththajaGalabodage@users.noreply.github.com>
2025-05-07 02:04:30 +0530

    implement use effect ✏️

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { sampleProducts } from "../../assets/sampleData";
+import axios from "axios";
 
 export default function AdminProductsPage() {
   const [products, setProducts] = useState(sampleProducts);
+
+  useEffect(() => {
+    axios
+      .get(import.meta.env.VITE_BACKEND_URL + "/api/products")
+      .then((res) => {
+        console.log(res.data);
+        setProducts(res.data);
+      });
+  }, []);
 
   return (
     <div className="w-full h-full  max-h-full overflow-y-scroll">
