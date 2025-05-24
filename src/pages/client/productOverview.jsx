95e7fc2c18fdf924b306fefda375799d0d08d38c
Chiththaja <162040367+ChiththajaGalabodage@users.noreply.github.com>
2025-05-24 12:38:13 +0530

    implement add cart 😇

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -4,6 +4,7 @@ import toast from "react-hot-toast";
 import { useParams } from "react-router-dom";
 import ImageSlider from "../../components/imageSlider";
 import Loading from "../../components/loading";
+import { addToCart, getCart } from "../../utils/cart";
 
 export default function ProductOverviewPage() {
   const params = useParams();
@@ -69,7 +70,17 @@ export default function ProductOverviewPage() {
               )}
 
               <div className="w-full flex justify-center items-center mt-4">
-                <button className="w-[200px] h-[50px] mx-4 cursor-pointer bg-accent text-white rounded-2xl hover:bg-accent/80 transition-all duration-300">
+                <button
+                  className="w-[200px] h-[50px] mx-4 cursor-pointer bg-accent text-white rounded-2xl hover:bg-accent/80 transition-all duration-300"
+                  onClick={() => {
+                    // localStorage.removeItem("cart");
+                    console.log("Old cart");
+                    console.log(getCart());
+                    addToCart(product, 1);
+                    console.log("New cart");
+                    console.log(getCart());
+                  }}
+                >
                   Add to Cart
                 </button>
                 <button className="w-[200px] h-[50px] mx-4 cursor-pointer bg-accent text-white rounded-2xl hover:bg-accent/80 transition-all duration-300">
