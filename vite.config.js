f8d88ed052d55370f808702c23414c8625981f24
Chiththaja <162040367+ChiththajaGalabodage@users.noreply.github.com>
2025-07-12 23:10:46 +0530

    f

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
+//454704357887-cirshapfhvtvve3en4q0svo5q53kbcec.apps.googleusercontent.com
+//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
