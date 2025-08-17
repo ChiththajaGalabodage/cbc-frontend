a4b00fd7613a2c9c11bb18e4b5b055a03c2bab9d
Chiththaja <162040367+ChiththajaGalabodage@users.noreply.github.com>
2025-08-17 23:44:47 +0530

    replace logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -22,7 +22,7 @@ export default function Header() {
         }}
         src="/logo.png"
         alt="Logo"
-        className="w-[80px] h-[80px] object-cover cursor-pointer"
+        className="mx-10 w-[110px] h-[80px] object-cover cursor-pointer"
       />
       <div className="w-[calc(100%-160px)] h-full  hidden md:flex justify-center items-center">
         <Link to="/" className=" text-[20px] font-bold mx-2">
