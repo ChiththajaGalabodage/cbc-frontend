a382db395ec03345f278933b0589ab8b571f024d
Chiththaja <162040367+ChiththajaGalabodage@users.noreply.github.com>
2025-08-18 00:56:17 +0530

    fix

--- src/pages/admin/addProductPage.jsx
+++ src/pages/admin/addProductPage.jsx
@@ -98,7 +98,7 @@ export default function AddProductPage() {
     }
   }
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center bg-green-800">
+    <div className="w-full h-full flex flex-col justify-center items-center ">
       <input
         type="text"
         placeholder="Product ID"
